58b45c28989f1cf238cc68c184d279f23e6b60ec
@Fawad228 <mfawadkhan05@gmail.com>
2025-08-11 19:30:18 +0500

    addlogo

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
+import HSQ from "../../public/HSQ.png";
 import {
   Users,
   Bed,
@@ -147,10 +148,7 @@ const Sidebar = ({ isOpen, onClose }) => {
         {/* Logo Section */}
         <div className="h-20 px-6 flex items-center border-b border-slate-800/50">
           <div className="flex items-center space-x-3">
-            <div className="relative">
-              <Crown className="h-9 w-9 text-amber-400" />
-              <Sparkles className="h-4 w-4 text-amber-300 absolute -top-1 -right-1" />
-            </div>
+            <img className="w-8 h-8  rounded-lg " src={HSQ} alt={HSQ} />
             <div>
               <h1 className="text-xl font-light tracking-wider text-white">
                 HSQ ADMIN
