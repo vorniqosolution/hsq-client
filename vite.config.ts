427a85fd4828104eef2711a101b80c528cdc8355
@Fawad228 <mfawadkhan05@gmail.com>
2025-08-26 15:54:13 +0500

    remove dockerfile from project

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
